e8a60e833d5d783e024135c8f84843e369e214a3
Santeri Sivula <santeri.sivula97@gmail.com>
2023-02-04 13:51:46 +0200

    Enemy spawing and skyboxing

---
+++ Assets/Scripts/Enemies/Spawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform TargetAround;
+
+    public float MinDistance = 5;
+    public float MaxDistance = 10;
+
+    public GameObject[] PrefabsToSpawn;
+
+    public float SpawnDelay = 1;
+    public int SpawnCount = 2;
+    public int SpawnIndex = 0;
+    private float curTime = 0;
+
+    private void Awake()
+    {
+        if (PrefabsToSpawn == null || PrefabsToSpawn.Length == 0)
+            gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        TargetAround = GlobalReferenceManager.Instance.Player.transform;
+    }
+
+    private void Update()
+    {
+        curTime += Time.deltaTime;
+
+        if (curTime < SpawnDelay)
+            return;
+
+        curTime = 0;
+
+        SpawnIndex++;
+        if (SpawnIndex == PrefabsToSpawn.Length)
+            SpawnIndex = 0;
+
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        for (int i = 0; i < SpawnCount; i++)
+        {
+            Vector3 randomPos = new Vector3(Random.value * 2 - 1, 0, Random.value * 2 - 1).normalized;
+            Vector3 target = randomPos * Mathf.Lerp(MinDistance, MaxDistance, Random.value) + TargetAround.position;
+            target.y = PrefabsToSpawn[SpawnIndex].transform.position.y;
+            Instantiate(PrefabsToSpawn[SpawnIndex], target, Quaternion.identity, transform);
+        }
+    }
+}
