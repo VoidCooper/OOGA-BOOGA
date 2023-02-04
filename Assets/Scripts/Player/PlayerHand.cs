6e579cae98f656b3e83bd5d05ad3a8e16c21d473
askol_varaani <asko.lahti@varaani.com>
2023-02-04 15:04:45 +0200

    Implemented throw animation

--- Assets/Scripts/Player/PlayerHand.cs
+++ Assets/Scripts/Player/PlayerHand.cs
@@ -7,21 +7,35 @@ public class PlayerHand : MonoBehaviour
     [Header("Projectile variables")]
     public float startVelocity = 5f;
     public float spearThrowCooldown = 0.2f;
+    public float spearThrowDelay = 0.02f;
     public GameObject spearPrefabObj;
     public Transform projectileSpawner;
+    public GameObject handModel;
 
     private bool _isThrowOnCooldown;
+    private Animator _animationComponent;
+
+    public void Awake()
+    {
+        _animationComponent = handModel.GetComponent<Animator>();
+    }
 
     public void ShootSpear()
     {
         if (_isThrowOnCooldown)
             return;
 
-        GameObject spearObj = GameObject.Instantiate(spearPrefabObj, projectileSpawner.position, projectileSpawner.rotation);
-        spearObj.GetComponent<SpearProjectile>().Launch();
+        _animationComponent.SetTrigger("ThrowSpearTrigger");
 
         _isThrowOnCooldown = true;
+        Invoke(nameof(InstantiateSpear), spearThrowDelay);
         Invoke(nameof(ResetThrowCooldown), spearThrowCooldown);
+    }
+
+    private void InstantiateSpear()
+    {
+        GameObject spearObj = GameObject.Instantiate(spearPrefabObj, projectileSpawner.position, projectileSpawner.rotation);
+        spearObj.GetComponent<SpearProjectile>().Launch();
     }
 
     private void ResetThrowCooldown()
