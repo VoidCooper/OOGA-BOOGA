0fe7fd153b5daeb5ca4180c583c4f55a8e725c3f
Sami Sipari <sami.sipari@tuni.fi>
2023-02-05 15:45:08 +0200

    Healing with starve is better!

--- Assets/Scripts/Player/Hunger.cs
+++ Assets/Scripts/Player/Hunger.cs
@@ -8,11 +8,14 @@ public class Hunger : MonoBehaviour
     public float CurrentHunger;
     public float StarveSpeed = 1f;
     public float StarveWhileHealing = 0.5f;
-    public float HealWithHungerSpeed = 0.25f;
+    public float HealWithHunger = 10f;
     public float DamageWithHungerSpeed = 1.5f;
 
     public event System.Action TookFood;
+    public event System.Action<bool> CanHealWithFood;
     private bool drainPaused = false;
+    private bool _canHealWithFood = true;
+    private ScaledRepeatingTimer _timer;
 
     Health health;
 
@@ -25,12 +28,15 @@ public class Hunger : MonoBehaviour
     {
         CurrentHunger = MaxHunger;
         health = GetComponent<Health>();
+        _timer = gameObject.AddComponent<ScaledRepeatingTimer>();
+        _timer.OnTimerCompleted += HealWithFood;
     }
 
     private void Start()
     {
         GameManager.Instance.OnGamePaused += OnGamePaused;
         GameManager.Instance.OnGameUnPaused += OnGameUnPaused;
+        _timer.StartTimer(2f);
     }
 
     private void OnDestroy()
@@ -51,17 +57,13 @@ public class Hunger : MonoBehaviour
 
     public void Update()
     {
+        if (GameManager.Instance.GamePaused)
+            return;
         if (drainPaused)
             return;
 
         float healAmount = 0;
         CurrentHunger -= StarveSpeed * Time.deltaTime;
-        if (health.CurrentHealth < health.MaxHealth)
-        {
-            healAmount -= HealWithHungerSpeed * Time.deltaTime;
-            CurrentHunger -= StarveWhileHealing * Time.deltaTime;
-        }
-
         CurrentHunger = Mathf.Clamp(CurrentHunger, 0, MaxHunger);
 
         if (CurrentHunger == 0)
@@ -69,7 +71,33 @@ public class Hunger : MonoBehaviour
             healAmount += DamageWithHungerSpeed * Time.deltaTime;
         }
 
+        if (CurrentHunger > (MaxHunger / 2))
+        {
+            _canHealWithFood = true;
+            CanHealWithFood?.Invoke(true);
+        }
+        else
+        {
+            _canHealWithFood = false;
+            CanHealWithFood?.Invoke(false);
+        }
+
+
         health.DealDamageWithoutScratch(healAmount);
+    }
+
+    public void HealWithFood()
+    {
+        if (_canHealWithFood && health.CurrentHealth < health.MaxHealth)
+        {
+            float healAmount = HealWithHunger;
+            if ((health.CurrentHealth + healAmount) > health.MaxHealth)
+                healAmount = health.MaxHealth - health.CurrentHealth;
+
+
+            CurrentHunger -= HealWithHunger;
+            health.DealDamage(-healAmount);
+        }
     }
 
     public void EatFood(float amount)
